7b159f2fb4c30b4d475727ad0a4c0b3f68e3e32a
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-06 17:30:55 +0530

    function declaration

--- Chapter-08/Strings/printLength.c
+++ Chapter-08/Strings/printLength.c
@@ -2,10 +2,18 @@
 
 // Q.1.-> Make a program that inputs user's name &prints its length.
 
+int printLength( char name[] );
+
+{
+    int i = 0;
+    for( i = 0; name[i] != '\0' ; i++ );
+    return i;
+}
 int main()
 {
     char name[100];
     fgets( name , 100 , stdin );
+
 
     return 0;
 }
