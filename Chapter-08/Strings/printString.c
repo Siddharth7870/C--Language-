aedc5e0532fc9d827b1c1fcdaf0a71989d09df73
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-06 15:04:07 +0530

    O/P : SIDDHARTH Ramanand

--- Chapter-08/Strings/printString.c
+++ Chapter-08/Strings/printString.c
@@ -5,8 +5,10 @@ void printString( char title[] );
 int main()
 {
     char title[] = {'S','I','D','D','H','A','R','T','H','\0'};
+    char FristName[] = "Ramanand";
     
     printString(title);
+    printString(FristName);
     return 0;
 }
 
