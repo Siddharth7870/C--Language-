9becad9e45d8c858d597b9061fadc1074559e73c
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-07 16:37:10 +0530

    After reverse

--- Chapter-08/Strings/strrev.c
+++ Chapter-08/Strings/strrev.c
@@ -4,6 +4,7 @@
 int main()
 {
     char str[100] = "Siddharth";
-    printf("Before reversing : %s\n",str);
+    printf("Before reversing : %s\n",str);// O/P : Siddharth
+    printf("After reversing : %s\n",strrev(str));// O/P : htrahddiS
     return 0;
 }
