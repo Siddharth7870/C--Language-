9b3d882083bdfbb70000433404acc60cfc29b636
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-07 18:20:49 +0530

    Declaration string variable

---
+++ Chapter-08/Strings/2.Practice.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+// Q.2.-> Take a string input from the user using %c .
+
+int main()
+{
+    char name[100];
+    char ch;
+    int i = 0;
+
+    return 0;
+}
