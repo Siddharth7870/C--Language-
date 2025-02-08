7ceab1e62ade3d1699d5604a5e50ff3db323cf83
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-08 16:10:04 +0530

    use logic

--- Chapter-08/Strings/5.Practice.c
+++ Chapter-08/Strings/5.Practice.c
@@ -14,5 +14,14 @@ int main()
 
 int countVowel( char vowel[ ] )
 {
-    
+    int count = 0;
+
+    for( int i = 0; vowel[ i ]  != '\0'; i++)
+    {
+        if( vowel[i] == 'a' || vowel[i] == 'e' || vowel[i] == 'i' || vowel[i] == 'o' || vowel[i] == 'u')
+        {
+            count++;
+        }
+    }
+    return count;
 }
