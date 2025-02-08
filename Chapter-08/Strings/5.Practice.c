f70fd3e7dbc7d9b12c4ca8fede700e744892776d
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-08 16:01:31 +0530

    function declaration & definition

--- Chapter-08/Strings/5.Practice.c
+++ Chapter-08/Strings/5.Practice.c
@@ -3,9 +3,16 @@
 
 // Q.5.-> Write a function to count the occurrence of vowels in a string.
 
+int countVowel( char vowel[] );
+
 int main()
 {   
     char vowel[ ] = "Hello World";
-    
+
     return 0;
+}
+
+int countVowel( char vowel[ ] )
+{
+    
 }
