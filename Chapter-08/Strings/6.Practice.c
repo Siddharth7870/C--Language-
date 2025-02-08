88c934a5754948f2caca3ecaa106a61d51f84d4a
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-08 18:50:43 +0530

    use 'for' loop and 'if' and 'print'

--- Chapter-08/Strings/6.Practice.c
+++ Chapter-08/Strings/6.Practice.c
@@ -15,5 +15,13 @@ int main()
 
 void checkChar( char str[ ], char ch )
 {
-    
+    for( int i = 0; str[ i ] != '\0'; i++ )
+    {
+        if( str[ i ] == ch )
+        {
+            printf("Character is present!");
+            return;
+        }
+    }
+    printf("Character is not present:");
 }
