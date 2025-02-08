abc857d2511f110749a6dbea10d79e84bb7440e9
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-08 14:56:46 +0530

    Function Declaration

--- Chapter-08/Strings/3.Practice.c
+++ Chapter-08/Strings/3.Practice.c
@@ -3,9 +3,12 @@
 
 // Q.3.-> Find the salted form of a password entered by user if the salt is "123" & added at the end.
 
+void salting( char password[] );
+
 int main()
 {   
     char password[ 100 ];
     scanf("%s",password);
+
     return 0;
 }
