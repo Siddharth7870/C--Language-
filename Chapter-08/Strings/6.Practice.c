b2d712b7d654dd2bc729d1aa690ed420d31891a1
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-08 18:42:25 +0530

    function declaration & definition

--- Chapter-08/Strings/6.Practice.c
+++ Chapter-08/Strings/6.Practice.c
@@ -3,9 +3,17 @@
 
 // Q.6.-> Check if a given character is present in a string or not .
 
+void checkChar( char str[ ], char ch );
+
 int main()
 {   
     char str[ ] = "Apnacollege";
     char ch = 'a';
+
     return 0;
+}
+
+void checkChar( char str[ ], char ch )
+{
+    
 }
