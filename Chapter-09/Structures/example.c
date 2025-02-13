b89c2d99438a7cf03b841843bca703f8f38f73a6
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-13 14:47:13 +0530

    structure syntax complet

--- Chapter-09/Structures/example.c
+++ Chapter-09/Structures/example.c
@@ -7,8 +7,14 @@ struct student
         int roll_no;
         float cgpa; 
      };
-     
+
 int main()
-{
+{   
+    struct student s1;
+    s1.name = "Ramanand kumar ";
+    s1.roll_no = 2422241;
+    s1.cgpa = 7.8;
+
+    
     return 0;
 }
