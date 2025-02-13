0f3b02a0a38826f0ddfb686d2fc5ab4e6661723e
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-13 14:56:35 +0530

    A example of structure

--- Chapter-09/Structures/example.c
+++ Chapter-09/Structures/example.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include<string.h>
+
 
 // user defined
 struct student
      {
-        char name;
+        char name[100];
         int roll_no;
         float cgpa; 
      };
@@ -11,10 +13,14 @@ struct student
 int main()
 {   
     struct student s1;
-    s1.name = "Ramanand kumar ";
+    //s1.name[100]= "Ramanand kumar ";
+    strcpy(s1.name,"Ramanand kumar");
     s1.roll_no = 2422241;
-    s1.cgpa = 7.8;
+    s1.cgpa = 8.8;
 
-    
+    printf("Student name = %s\n",s1.name);
+    printf("Student roll no = %d\n",s1.roll_no);
+    printf("Student cgpa = %.2f\n",s1.cgpa);
+
     return 0;
 }
