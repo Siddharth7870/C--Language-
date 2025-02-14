a09a2974e5bf5f2b50cc073afde654a3b56ec03d
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-14 19:04:12 +0530

    some add

--- Chapter-08/Strings/exStrcmp.c
+++ Chapter-08/Strings/exStrcmp.c
@@ -5,7 +5,10 @@ int main()
 {
     char firStr[] = "HHC";
     char secStr[] = "HHB";
-    int result = strcmp(firStr , secStr);
-    printf("%d\n",result);// O/P : 1
+    //int result = strcmp(firStr , secStr);
+    //printf("%d\n",result);// O/P : 1
+    // Or
+    printf("Comperetion  = %d ",strcmp( firStr, secStr));
+    
     return 0;
 }
